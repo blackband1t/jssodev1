b918907bac43597ec71ac9b13a8ff68cf4a3c801
Ozan Tekin <ozantekin.95@windowslive.com>
2022-09-06 09:21:31 +0300

    first commit

---
+++ js/asd.js
@@ -0,0 +1,66 @@
+let name = prompt("İsminizi Giriniz")
+let demo = document.querySelector("#demo")
+
+demo.innerHTML = `${name}`
+
+// function showTime(){
+//     var date = new Date();
+//     var h = date.getHours(); // 0 - 23
+//     var m = date.getMinutes(); // 0 - 59
+//     var s = date.getSeconds(); // 0 - 59
+//     var session = "AM";
+    
+//     if(h == 0){
+//         h = 12;
+//     }
+    
+//     if(h > 12){
+//         h = h - 12;
+//         session = "PM";
+//     }
+    
+//     h = (h < 10) ? "0" + h : h;
+//     m = (m < 10) ? "0" + m : m;
+//     s = (s < 10) ? "0" + s : s;
+    
+//     var time = h + ":" + m + ":" + s + " " + session;
+//     document.getElementById("MyClockDisplay").innerText = time;
+//     document.getElementById("MyClockDisplay").textContent = time;
+    
+//     setTimeout(showTime, 1000);
+    
+// }
+
+// showTime();
+function startTime() {
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+    hr = (hr == 0) ? 12 : hr;
+    hr = (hr > 12) ? hr - 12 : hr;
+    //Add a zero in front of numbers<10
+    hr = checkTime(hr);
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+    
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+    document.getElementById("date").innerHTML = date;
+    
+    var time = setTimeout(function(){ startTime() }, 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+startTime();
